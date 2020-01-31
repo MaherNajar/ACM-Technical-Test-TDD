412df0cf9e12ad4b04b7c34c54269f64de82b875
maher <mahernajar87@gmail.com>
2020-01-31 11:32:35 +0100

    + Console project .Net Core 2.1

---
+++ ACM_Technical_Test.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace ACM_Technical_Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Arrange
+            string[] data = { "10101", "11100", "11010", "00101" };
+            var manager = new TeamManager(data);
+
+            // Act
+            var result = manager.GetBestTeams();
+
+            // Assert
+            Contract.Assert(result[0] == 5);
+            Contract.Assert(result[1] == 2);
+        }
+    }
+}
